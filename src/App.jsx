f865b1e801b21c5a961eee9b9d8d474c534eee90
Shwetank <mshwetank@gmail.com>
2025-12-05 09:43:20 +0530

    Started digital nomads

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import First from './components/firstComponent/First'
+import FirstAbout from './components/firstComponent/FirstAbout'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <First />
+      <FirstAbout />
     </div>
   )
 }
---
+++ src/components/firstComponent/FirstAbout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FirstAbout = () => {
+  return (
+    <>
+        <div className='p-5'>
+            <div>
+                <h1>Digital Marketing Sunshine coast</h1>
+            </div>
+
+            <div>
+                
+            </div>
+        </div>
+        
+        <div>
+
+        </div>
+    </>
+  )
+}
+
+export default FirstAbout
