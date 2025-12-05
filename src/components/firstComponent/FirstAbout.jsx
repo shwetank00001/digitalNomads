a80eddf1ec637cfd3432601c0f24cec599273306
Shwetank <mshwetank@gmail.com>
2025-12-05 12:58:21 +0530

    Specialist done

--- src/components/firstComponent/FirstAbout.jsx
+++ src/components/firstComponent/FirstAbout.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const FirstAbout = () => {
   return (
-    <div className='bg-[#111111] px-10 py-15 md:px-20 md:py-40 lg:px-50'>
+    <div className='bg-[#111111] px-5 py-10 sm:px-10 xl:px-50 3xl:px-100 lg:py-40 gap-20 '>
       <div className='gap-10 text-white 2xl:flex items-center justify-center'>
           <div className=''>
               <div>
-                  <p className='uppercase text-xs sm:text-md md:text-lg'>Digital Marketing Sunshine coast</p>
+                  <p className='uppercase text-xs sm:text-md md:text-md'>Digital Marketing Sunshine coast</p>
               </div>
               <br/>
               <div>
-                  <p className='text-4xl font-semibold'>Marketing isn't about shouting the loudest; <span className='text-[#F34D01]'>it's about resonating the deepest...</span></p>
+                  <p className='text-5xl font-semibold'>Marketing isn't about shouting the loudest; <span className='text-[#F34D01]'>it's about resonating the deepest...</span></p>
               </div>
           </div>
           <br/>
--- src/components/firstComponent/Specialist.jsx
+++ src/components/firstComponent/Specialist.jsx
@@ -1,24 +1,27 @@
 import Specialist1 from '../../../public/Specialist1.jpg'
 import Specialist2 from '../../../public/Specialist2.jpg'
 import Specialist3 from '../../../public/Specialist3.jpg'
+import { Button } from "@/components/ui/button"
+
 
 const Specialist = () => {
   return (
-    <section className='px-5 py-10 sm:px-10 xl:px-50 md:py-40 flex gap-10 '>
-        <div className='p-5 gap-10 flex flex-col '>
-          <div>
-            <img className='rounded-2xl w-50%' src={Specialist1} />
-          </div>
-            <div className='flex gap-5'>
-              <img className='rounded-2xl w-73' src={Specialist2} />
-              <img className='rounded-2xl w-73' src={Specialist3} />
-            </div>
-
-        </div>
-
+    <section className='px-5 py-10 sm:px-10 xl:px-50 3xl:px-100 lg:py-20 xl:py-40 flex flex-col items-center '>
+      <div className='sm:flex gap-20 '>
+      <div>
         <div>
-            <p className='text-md uppercase text-[#F34D01]'>a team of specialists... not generalists</p>
-            <p className='text-5xl font-semibold'>Local experts, providing honest digital marketing</p>
+          <img src={Specialist1} className='rounded-2xl'/>
+        </div>
+        <div className='flex gap-1 pt-5 '>
+          <img className='w-[50%] h-[50%] rounded-2xl' src={Specialist2} />        
+          <img className='w-[50%] h-[50%] rounded-2xl' src={Specialist3} />        
+               
+        </div>
+      </div>
+
+        <div className='mt-5'>
+            <p className='text-xs md:text-md lg:text-lg text-md uppercase text-[#F34D01]'>a team of specialists... not generalists</p>
+            <p className='text-3xl lg:text-5xl font-semibold'>Local experts, providing honest digital marketing</p>
             <br />
             <p className='text-lg'>Work with our incredible team of local talent… right here on the Sunshine Coast! Let Digital Nomads HQ be your creative partner in digital marketing. With our combined expertise and your vision – that’s the formula for success. We’ll tailor a strategy that blends our services to reach your target audience and drive growth. Being one of the fastest-growing digital marketing agencies on the Sunshine Coast, know that <span className='underline font-semibold'>we have the expertise to deliver</span> on your next marketing campaign.</p>
                         <br />
@@ -26,6 +29,11 @@ const Specialist = () => {
             <br />
             <hr />
         </div>
+      </div>
+
+      <div className='mt-5'>
+        <Button className="rounded-4xl p-8 border-2" variant="outline">See our awards...</Button>
+      </div>
     </section>
   )
 }
